3328d4b4da012695ca3e0641f00d3b5ca15d1a59
ksentar <ksentarius@yandex.ru>
2021-07-22 22:49:32 +0500

    promise interval

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const fs=require("fs/promises");
+const fs = require("fs/promises");
+const timeout = (ms) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      console.log("TIMER");
+    }, ms);
+  });
 
-const main=async()=>{
-    const json =await fs.readFile('sample.json');
-    console.log(json);
+const interval = (ms) =>
+  new Promise((resolve, reject) => {
+    setInterval(() => {
+      resolve();
+      console.log("TIMER");
+    }, ms);
+  });
+const main = async () => {
+  await interval(1000);
+  await timeout(5000);
+  const json = await fs.readFile("sample.json");
+  console.log(json);
 };
 main();
 
